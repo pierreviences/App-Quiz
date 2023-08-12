9b4105972b4f8546c3c81f04bcb08d025fdc1129
pierreviences <imaniyazid@gmail.com>
2023-08-12 10:06:19 +0700

    feat: create question, option, answer array and question index also score values

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -9,6 +9,26 @@ import com.example.quizapp.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
+    // step 2 -> create question, option, and answer array
+    private val questions = arrayOf("what is the built-in database in Andorid Studio?",
+        "What is the full from of APK in Android Development?",
+        "In Which year, frist andorid was relesead by Google?")
+
+    private val options = arrayOf(arrayOf("MySQL", "SQLite", "FireBase"),
+        arrayOf("Aplication Programing Interace", "Android programming interface", "Andori Package Information")
+        ,arrayOf("2010", "2006", "2008"))
+
+    private val correctAnswer = arrayOf(1,0, 2)
+
+    // step 3 -> create question index and score value
+    private var currecntQuestionIndex = 0
+    private var score = 0
+
+
+
+
+
+
 
 
 
@@ -24,5 +44,6 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-  
+   
+
 }
